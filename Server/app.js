65b1530398805ce231714733ac0262987e53ae76
xkdlsxm2 <jhjh92@naver.com>
2015-07-01 03:12:22 +0900

    5

--- Server/app.js
+++ Server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/contents',contents);
+app.use('/contents', contents);
 app.use('/thumbnails', thumbnails);
 
 // catch 404 and forward to error handler
